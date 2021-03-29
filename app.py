56e4d50fc6e159b7cace13be0d20b1289103bb28
komatsuyama yuuto <y.komatsuyama.sys20@morijyobi.ac.jp>
2021-03-29 11:28:24 +0900

    給与計算アプリの新規作成
    給与計算アプリ用に
    app.pyの更新
    salary.htmlの新規作成
    salary_resultの新規作成をしました。

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def top():
     return render_template("index.html")
+
+@app.route("/salary")
+def salary():
+    return render_template("salary.html")
+
+@app.route("/salary_result")
+def salary_result():
+    salary = request.args.get("salary")
+    time = request.args.get("time")
+
+    salary = int(salary) * int(time)
+
+    return render_template("salary_result.html", salary=salary)
 
 if __name__ == "__main__":
     app.run(debug=True)
