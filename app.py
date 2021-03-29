ff3e45bb984e9a3d3f68b150f224326da9a14fed
kodama ryosuke <r.kodama.sys20@morijyobi.ac.jp>
2021-03-30 00:16:05 +0900

    [add] 円面積計算処理の追加
    ・index.htmlにリンクの追記
    ・app.pyに面積を求める処理の追記
    ・calcarea_form.htmlの新規作成

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , redirect , url_for , request
 
 app = Flask(__name__)
 
 @app.route("/")
 def top():
     return render_template("index.html")
+
+#? 円の面積計算=================================================================
+@app.route("/calcarea_form")
+def calcarea_form():
+    reuslt = request.args.get("result")
+    return render_template("calcarea_form.html", result=reuslt)
+
+@app.route("/calc_result")
+def calc_result():
+    radius = request.args.get("radius")
+    if radius == None or radius == "":
+        return redirect(url_for("calcarea_form", result="error"))
+    result = int(radius) * int(radius) * 3.14
+    return redirect(url_for("calcarea_form", result=result))
+#? ｺｺﾏﾃﾞ=======================================================================
 
 if __name__ == "__main__":
     app.run(debug=True)
